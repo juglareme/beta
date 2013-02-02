2fe401a361c002220b0db19a31049ad32acccf9c
josemanuel @juglareme <juglareme@gmail.com>
2013-02-02 15:08:46 -0800

    A

---
+++ pseudo acciones.js
@@ -0,0 +1,71 @@
+//inicio de script marca todos disponibles agregables
+
+
+function marcar disponible(numero){
+rect.add function clic agregar a cesta
+
+
+}
+
+
+//
+
+
+
+//inicio de Javascript, llamada a diferentes Ajax.
+
+// Carga datos particulares,paso parametro desde php en base a ssesion.
+
+
+
+function obten_asignados(id_cliente){
+
+
+Obtengo_datos:string;  //separado por comas desde Ajax via PHP o python
+
+texto, substring, 
+
+//llamada a funcion de marcado, llenado de cesta 
+
+for texto until longitud; 
+
+marcado_actual(numero);
+agregar_cesta(numero);
+
+end for
+
+
+}
+
+
+
+
+
+
+//------------------------------------------------Marcado
+
+marcado_actual(numero){
+marca_color.css
+
+}
+agregar_cesta(numero){
+agregar a list:add element
+
+}
+
+
+
+//--------------------------------listen events
+
+
+<label onClick>Agregar </label>
+
+
+
+<label onClick>Eliminar </label>
+<label onClick>Limpiar </label>
+
+
+
+
+
